ecf2a1b2d486b9c48f392771724d92ca73a0116e
marinalaym <mlaforgithub@gmail.com>
2024-12-02 12:33:11 +0300

    Update procedures fragment

---
+++ VetClinic/app/src/main/java/ru/marinalyamina/vetclinic/adapters/ProcedureAdapter.kt
@@ -0,0 +1,54 @@
+package ru.marinalyamina.vetclinic.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import ru.marinalyamina.vetclinic.R
+import ru.marinalyamina.vetclinic.models.Procedure
+
+class ProcedureAdapter(private val context: Context, private val procedures: List<Procedure>) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    companion object {
+        private const val TYPE_HEADER = 0
+        private const val TYPE_ITEM = 1
+    }
+
+    class ProcedureViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val nameTextView: TextView = itemView.findViewById(R.id.tvProcedureName)
+        val priceTextView: TextView = itemView.findViewById(R.id.tvProcedurePrice)
+    }
+
+    class HeaderViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun getItemViewType(position: Int): Int {
+        return if (position == 0) TYPE_HEADER else TYPE_ITEM
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return if (viewType == TYPE_HEADER) {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_header_procedures, parent, false)
+            HeaderViewHolder(view)
+        } else {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_procedure, parent, false)
+            ProcedureViewHolder(view)
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is ProcedureViewHolder) {
+            val procedure = procedures[position - 1]
+            holder.nameTextView.text = procedure.name
+            holder.priceTextView.text = String.format("%d", procedure.price)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return procedures.size + 1
+    }
+}
--- VetClinic/app/src/main/java/ru/marinalyamina/vetclinic/fragments/ProceduresFragment.kt
+++ VetClinic/app/src/main/java/ru/marinalyamina/vetclinic/fragments/ProceduresFragment.kt
@@ -1,60 +1,30 @@
 package ru.marinalyamina.vetclinic.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import ru.marinalyamina.vetclinic.R
+import ru.marinalyamina.vetclinic.adapters.ProcedureAdapter
+import ru.marinalyamina.vetclinic.models.Procedure
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class ProceduresFragment : Fragment(R.layout.fragment_procedures) {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ProceduresFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class ProceduresFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var recyclerView: RecyclerView
+    private val proceduresList = listOf(
+        Procedure("Процедура 1 test test test test test test test test test", 500),
+        Procedure("Процедура 2 test test test test", 1200),
+        Procedure("Процедура 3", 800),
+        Procedure("Процедура 4test test test", 450)
+    )
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_procedures, container, false)
-    }
+        recyclerView = view.findViewById(R.id.recyclerViewProcedures)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProceduresFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProceduresFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        recyclerView.adapter = ProcedureAdapter(requireContext(), proceduresList)
     }
 }
