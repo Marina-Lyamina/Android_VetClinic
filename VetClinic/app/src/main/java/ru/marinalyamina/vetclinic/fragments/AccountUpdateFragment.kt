50695adfdbe4b9814bbce683296575cdc21727fe
marinalaym <mlaforgithub@gmail.com>
2024-12-27 09:33:58 +0300

    fixes

--- VetClinic/app/src/main/java/ru/marinalyamina/vetclinic/fragments/AccountUpdateFragment.kt
+++ VetClinic/app/src/main/java/ru/marinalyamina/vetclinic/fragments/AccountUpdateFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.*
 import androidx.annotation.RequiresApi
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import ru.marinalyamina.vetclinic.R
 import ru.marinalyamina.vetclinic.api.ApiService
@@ -27,6 +28,8 @@ class AccountUpdateFragment : Fragment(R.layout.fragment_account_update) {
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        updateToolbarTitle(getString(R.string.title_account_update))
 
         val editTextName: EditText = view.findViewById(R.id.firstNameInput)
         val editTextSurname: EditText = view.findViewById(R.id.lastNameInput)
@@ -145,5 +148,10 @@ class AccountUpdateFragment : Fragment(R.layout.fragment_account_update) {
                 Toast.makeText(requireContext(), "Ошибка подключения", Toast.LENGTH_SHORT).show()
             }
         })
+    }
+
+    private fun updateToolbarTitle(title: String) {
+        val toolbar = requireActivity().findViewById<Toolbar>(R.id.toolbar)
+        toolbar.title = title
     }
 }
